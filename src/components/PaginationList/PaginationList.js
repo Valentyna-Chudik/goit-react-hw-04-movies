359cd40c5c6119a05c04389d2adef6a19031a47a
Valentyna-Chudik <valentyna_chudik@ukr.net>
2021-03-23 13:20:38 +0200

    Pagination style overrided, favicon added

--- src/components/PaginationList/PaginationList.js
+++ src/components/PaginationList/PaginationList.js
@@ -1,15 +1,38 @@
 import { Pagination } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      display: 'flex',
+      justifyContent: 'center',
+      '& .MuiPaginationItem-root': {
+        color: '#e5e5e5',
+        textShadow: '1px 3px 2px rgb(0 0 0 / 40%)',
+      },
+      '& .MuiPaginationItem-page.Mui-selected': {
+        backgroundColor: '#2371b1',
+      },
+    },
+  },
+}));
 
 export default function PaginationList({ page, totalPages, handleChange }) {
+  const classes = useStyles();
   return (
     <>
       {totalPages > 1 && (
         <Pagination
+          className={classes.root}
           page={page}
           count={totalPages}
           onChange={handleChange}
           color="primary"
           size="large"
+          showFirstButton
+          showLastButton
         />
       )}
     </>
